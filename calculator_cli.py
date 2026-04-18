01df8dca813ddda33a89f9147c6587248514bf19
dateac-cloud <dateamitc@gmail.com>
2026-04-18 19:08:00 +0530

    Create calculator_cli.py

---
+++ calculator_cli.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+print("Addition:", add(5, 3))
+print("Subtraction:", sub(5, 3))
+print("Multiplication:", mul(5, 3))
+print("Division:", div(6, 3))
